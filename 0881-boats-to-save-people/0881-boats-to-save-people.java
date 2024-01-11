fed3dda70532784f0c4bfc01ab5269fef8d77ec2
faizagha <71570366+faizagha@users.noreply.github.com>
2024-01-11 18:11:42 +0530

    Time: 18 ms (15.48%), Space: 53.4 MB (56.20%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,31 +1,19 @@
-import java.util.Arrays;
-
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        // Case where only 1 boat is needed
-        int sum = Arrays.stream(people).sum();
-        if (sum <= limit) {
-            return 1;
-        }
-
         Arrays.sort(people);
         int a = 0;
         int b = people.length - 1;
         int boatCount = 0;
-
-        while (a <= b) {
+        while (a <= b){
             int peopleSum = people[a] + people[b];
-            if (peopleSum > limit) {
+            if (peopleSum > limit){
                 boatCount++;
                 b--;
-            } else {
-                // Use a single boat for both people if their sum is within the limit
+            } else if (peopleSum <= limit) {
                 boatCount++;
-                a++;
-                b--;
+                a++;b--;
             }
         }
-
         return boatCount;
     }
 }

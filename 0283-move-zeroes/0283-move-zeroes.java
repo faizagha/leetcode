0e4ce1e54b6fea23243398801fc2d16539f9414b
faizagha <71570366+faizagha@users.noreply.github.com>
2024-01-12 04:38:05 +0530

    Time: 1 ms (100.00%), Space: 46 MB (25.49%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-    int[] zeroMoved = new int[nums.length];
-    int index = 0;
+        int[] zeroMoved = new int[nums.length];
+        int index = 0;
 
-    for (int num : nums) {
-        if (num != 0) {
-            zeroMoved[index] = num;
-            index++;
+        for (int num : nums) {
+            if (num != 0) {
+                zeroMoved[index] = num;
+                index++;
+            }
         }
-    }
 
-    // Copy the elements from zeroMoved back to nums
-    for (int i = 0; i < nums.length; i++) {
-        nums[i] = zeroMoved[i];
+        // Copy the elements from zeroMoved back to nums
+        System.arraycopy(zeroMoved, 0, nums, 0, nums.length);
     }
-}
 
 }
